bfe51650393c48485ef8944a94dd57248cae665b
eltonlin <eltonlin@umich.edu>
2019-12-29 00:23:54 -0500

    added copyright info

--- AlgorithmLibrary/DequeArray.js
+++ AlgorithmLibrary/DequeArray.js
@@ -1,15 +1,15 @@
 // Copyright 2011 David Galles, University of San Francisco. All rights reserved.
-//
+
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
-//
+
 // 1. Redistributions of source code must retain the above copyright notice, this list of
 // conditions and the following disclaimer.
-//
+
 // 2. Redistributions in binary form must reproduce the above copyright notice, this list
 // of conditions and the following disclaimer in the documentation and/or other materials
 // provided with the distribution.
-//
+
 // THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR IMPLIED
 // WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 // FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
@@ -19,7 +19,7 @@
 // ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
+
 // The views and conclusions contained in the software and documentation are those of the
 // authors and should not be interpreted as representing official policies, either expressed
 // or implied, of the University of San Francisco
@@ -38,7 +38,7 @@
 // allocating pointers (fixed-size arrays) dynamically by preallocate a constant number of arrays
 // to achieve the desired animation result and ease of control over arrays elements and index.
 // To reiterate, this is not an appropriate way to implement an actual deque. A circular buffer 
-// would probably fit in Javascript better, but not as cool in my opinion.
+// would probably fit wtih Javascript better, but not as cool in my opinion.
 
 var ARRAY_START_X = 330;
 var ARRAY_START_Y = 140;
